9c0633b042055eef395cf975b74746b9fb9599a3
ilong <wenlong.li@vhall.com>
2017-03-29 18:24:14 +0800

    test

--- HelloWorld/python_test.py
+++ HelloWorld/python_test.py
@@ -1,1 +1,20 @@
-print('hello world python!');
+
+print('hello world python!')
+
+def my_abs(x):
+    if x>0:
+        return x
+    else:
+        return -x
+
+y = my_abs(-100)
+print("y:"+str(y))
+
+classmates = ["Michael", 'Jack', 'Bob', 'Tracy']
+name = classmates.pop(1)
+print("name:"+name)
+print(classmates)
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print("score:"+str(d["Bob"]))
+print(d)
